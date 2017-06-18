a7aa1e201fe44cd848d61c9e6952a095889784cc
portkeys <portkeys@portkeyss-MacBook-Air.local>
2017-06-18 12:33:09 -0700

    Mini Project Code

---
+++ mini_projects/2_svm_author_id.py
@@ -0,0 +1,58 @@
+#!/usr/bin/python
+
+"""
+    This is the code to accompany the Lesson 2 (SVM) mini-project.
+
+    Use a SVM to identify emails from the Enron corpus by their authors:
+    Sara has label 0
+    Chris has label 1
+"""
+
+import sys
+from time import time
+sys.path.append("../tools/")
+from email_preprocess import preprocess
+import numpy as np
+
+
+### features_train and features_test are the features for the training
+### and testing datasets, respectively
+### labels_train and labels_test are the corresponding item labels
+features_train, features_test, labels_train, labels_test = preprocess()
+
+
+
+
+#########################################################
+### your code goes here ###
+from sklearn.svm import SVC
+#clf = SVC(kernel = 'linear')
+clf = SVC(kernel = 'rbf', C=10000)
+
+# smaller training datasets
+#features_train = features_train[:len(features_train)/100]
+#labels_train = labels_train[:len(labels_train)/100]
+
+t0 = time()
+clf.fit(features_train, labels_train)
+print "training time:", round(time()-t0,3),"s"
+
+t1 = time()
+pred = clf.predict(features_test)
+print "prediction time:", round(time()-t1,3),"s"
+
+# print accuracy
+from sklearn.metrics import accuracy_score
+acc = accuracy_score(labels_test, pred)
+print acc
+
+# Extracting Predictions from an SVM
+print pred[10]
+print pred[26]
+print pred[50]
+
+# How many Chris Emails Predicted
+print np.count_nonzero(pred==1)
+
+
+#########################################################
---
+++ mini_projects/4_adaboost_terrain_data.py
@@ -0,0 +1,51 @@
+#!/usr/bin/python
+
+import matplotlib.pyplot as plt
+from prep_terrain_data import makeTerrainData
+from class_vis import prettyPicture
+
+features_train, labels_train, features_test, labels_test = makeTerrainData()
+
+
+### the training data (features_train, labels_train) have both "fast" and "slow"
+### points mixed together--separate them so we can give them different colors
+### in the scatterplot and identify them visually
+grade_fast = [features_train[ii][0] for ii in range(0, len(features_train)) if labels_train[ii]==0]
+bumpy_fast = [features_train[ii][1] for ii in range(0, len(features_train)) if labels_train[ii]==0]
+grade_slow = [features_train[ii][0] for ii in range(0, len(features_train)) if labels_train[ii]==1]
+bumpy_slow = [features_train[ii][1] for ii in range(0, len(features_train)) if labels_train[ii]==1]
+
+
+#### initial visualization
+plt.xlim(0.0, 1.0)
+plt.ylim(0.0, 1.0)
+plt.scatter(bumpy_fast, grade_fast, color = "b", label="fast")
+plt.scatter(grade_slow, bumpy_slow, color = "r", label="slow")
+plt.legend()
+plt.xlabel("bumpiness")
+plt.ylabel("grade")
+plt.show()
+################################################################################
+
+
+### your code here!  name your classifier object clf if you want the
+### visualization code (prettyPicture) to show you the decision boundary
+
+import time
+from sklearn import(ensemble, tree, metrics)
+
+clf = ensemble.AdaBoostClassifier(algorithm="SAMME",
+base_estimator=tree.DecisionTreeClassifier(min_samples_split=15),
+learning_rate=0.28,
+n_estimators=100)
+
+clf = clf.fit(features_train,labels_train)
+pred = clf.predict(features_test)
+acc = round(metrics.accuracy_score(labels_test, pred),3)
+print 'Accuracy of AdaBoost:',acc
+
+
+try:
+    prettyPicture(clf, features_test, labels_test)
+except NameError:
+    pass
